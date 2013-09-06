637e363dd8136d4d64825d62bfa3962aa48feda0
antriver <Anthony@Anthonys-MacBook-Pro.local>
2013-09-06 15:44:11 +0800

    Initial commit

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version = 2013090601; // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2012020800; // Requires this Moodle version
+$module->component = 'mod_brainpop'; // Full name of the plugin (used for diagnostics)
+$module->cron = 0;

f8db82caa66ae9577454f02e9f81f4083e279d39
Anthony Kuske <anthonykuske@gmail.com>
2016-01-06 16:45:26 +0000

    Update version for moodle 30

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version = 2014112600; // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2012020800; // Requires this Moodle version
-$module->component = 'mod_brainpop'; // Full name of the plugin (used for diagnostics)
-$module->cron = 0;
+$plugin->version = 2016010100; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2012020800; // Requires this Moodle version
+$plugin->component = 'mod_brainpop'; // Full name of the plugin (used for diagnostics)
+$plugin->cron = 0;
